24eb4cb8abcc8ba197acc23d5be4b8927f1cbb4f
瑞雯 <liuwch@yoozoo.com>
2021-10-05 10:20:04 +0800

    [update] stream

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
-	"haha/conf"
-	"haha/entity"
-
-	"github.com/reugn/go-streams"
-	"github.com/reugn/go-streams/flow"
 	"github.com/urfave/cli"
 )
 
@@ -27,47 +21,5 @@ func main() {
 }
 
 func do(ctx *cli.Context) error {
-	confPath := ctx.String("conf")
-	if len(confPath) == 0 {
-		return errors.New("no config path")
-	}
-	return nil
-}
-
-func run(config conf.StreamConfig) error {
-	for _, agg := range config.AggConfigs {
-		sourceConfig, ok := config.FindEntityConfig(agg.SourceId)
-		if !ok {
-			return errors.New("source not found")
-		}
-		sinkConfig, ok := config.FindEntityConfig(agg.SinkId)
-		if !ok {
-			return errors.New("sink not found")
-		}
-		source, ok := entity.GetSourceEntity(sourceConfig)
-		if !ok {
-			return errors.New("source not found")
-		}
-		sink, ok := entity.GetSinkEntity(sinkConfig)
-		if !ok {
-			return errors.New("sink not found")
-		}
-
-		var (
-			f streams.Flow = flow.NewPassThrough()
-		)
-		for i, flowConfig := range agg.FlowConfigs {
-			f, ok = entity.GetFlow(flowConfig)
-			if !ok {
-				return errors.New("source not found")
-			}
-			if i == 0 {
-				continue
-			}
-			f = f.Via(f)
-		}
-
-		source.Via(f).To(sink)
-	}
 	return nil
 }
