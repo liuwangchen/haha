0fef5210209e65a16db4ad617d2e20faca52156b
瑞雯 <liuwch@yoozoo.com>
2021-11-13 18:40:16 +0800

    batch support

--- main.go
+++ main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/urfave/cli"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
-func main() {
-	app := cli.NewApp()
-	app.EnableBashCompletion = true
-	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "conf", Usage: "配置", Value: "./stream.yaml"},
-	}
-	app.Action = do
-	err := app.Run(os.Args)
+// NewProxy takes target host and creates a reverse proxy
+func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
+	url, err := url.Parse(targetHost)
 	if err != nil {
-		fmt.Println("err", err)
+		return nil, err
 	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	originalDirector := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		originalDirector(req)
+	}
+
+	return proxy, nil
 }
 
-func do(ctx *cli.Context) error {
-	return nil
+func main() {
+	// initialize a reverse proxy and pass the actual backend server url here
+	proxy, err := NewProxy("http://172.25.143.197:10000")
+	if err != nil {
+		panic(err)
+	}
+
+	// handle all requests to your server using the proxy
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
